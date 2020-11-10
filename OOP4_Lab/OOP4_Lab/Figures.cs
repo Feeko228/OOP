bd75851732b227446922e8546ccab673d3274c35
Feeko228 <45923367+Feeko228@users.noreply.github.com>
2020-11-10 13:13:51 +0300

    lab 4 added

---
+++ OOP4_Lab/OOP4_Lab/Figures.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace OOP4_Lab
+{
+    class Figures
+    {
+        List<IShape> figures = new List<IShape>();
+        public List<IShape> RetShape()
+        {
+            return figures;
+        }
+        public IShape this[int i]
+        {
+            get { return figures[i]; }
+        }
+
+        public void AddShape(IShape shape)
+        {
+            figures.Add(shape);
+        }
+        public double AllAreaSumm()
+        {
+            double num = 0;
+            foreach (IShape a in figures)
+                num += a.CalcArea();
+            return num;
+        }
+        public double AllPerimSumm()
+        {
+            double num = 0;
+            foreach (IShape a in figures)
+                num += a.CalcPerim();
+            return num;
+        }
+        public IShape MaxArea()
+        {
+            double max = 0;
+            IShape found = null;
+            foreach (IShape a in figures)
+                if (a.CalcPerim() > max)
+                {
+                    max = a.CalcArea();
+                    found = a;
+                }
+            return found;
+        }
+        public IShape MinArea()
+        {
+            double min = 1.7976931348623158e+308;
+            IShape found = null;
+            foreach (IShape a in figures)
+                if (a.CalcPerim() < min)
+                {
+                    min = a.CalcArea();
+                    found = a;
+                }
+            return found;
+        }
+        public IShape MaxPerim()
+        {
+            double max = 0;
+            IShape found = null;
+            foreach (IShape a in figures)
+                if (a.CalcPerim() > max)
+                {
+                    max = a.CalcPerim();
+                    found = a;
+                }
+            return found;
+        }
+        public IShape MinPerim()
+        {
+            double min = 1.7976931348623158e+308;
+            IShape found = null;
+            foreach (IShape a in figures)
+                if (a.CalcPerim() < min)
+                {
+                    min = a.CalcPerim();
+                    found = a;
+                }
+            return found;
+        }
+    }
+    interface IShape
+    {
+        double CalcArea();
+        double CalcPerim();
+        string ToString();
+    }
+    class Square : IShape
+    {
+        double SideLength;
+        public Square(Point p1, Point p2) { SideLength = Math.Abs(p1.Y - p2.Y); }
+        public double CalcArea() { return Math.Pow(SideLength, 2); }
+        public double CalcPerim() { return SideLength * 4; }
+        public override string ToString() { return $"Perimeter of Square is {CalcPerim()} and Area is {CalcArea()}"; }
+    }
+    class Rectangle : IShape
+    {
+        private double LeftSide;
+        private double BottomSide;
+        public Rectangle(Point p1, Point p2)
+        {
+            LeftSide = Math.Abs(p1.Y - p2.Y);
+            BottomSide = Math.Abs(p1.X - p2.X);
+        }
+        public double CalcArea() { return LeftSide * BottomSide; }
+        public double CalcPerim() { return (LeftSide * 2) + (BottomSide * 2); }
+        public override string ToString() { return $"Perimeter of Rectangle is {CalcPerim()} and Area is {CalcArea()}"; }
+    }
+    class Circle : IShape
+    {
+        private double Radius;
+        public Circle(Point p1, Point p2) { Radius = Math.Abs(p1.X - p2.X); }
+        public double CalcArea() { return 3.14 * Math.Pow(Radius, 2); }
+        public double CalcPerim() { return 2 * Radius * 3.14; }
+        public override string ToString() { return $"Perimeter of Circle is {CalcPerim()} and Area is {CalcArea()}"; }
+
+    }
+    class Triangle : IShape
+    {
+        private int side_r_length, side_l_length, bottom_length;
+        public double CalcArea()
+        {
+            return Math.Sqrt(CalcPerim() / 2 * (CalcPerim() / 2 - side_r_length) * (CalcPerim() / 2 - side_l_length) * (CalcPerim() / 2 - bottom_length));
+        }
+        public double CalcPerim() { return side_r_length + side_l_length + bottom_length; }
+        public Triangle(Point p1, Point p2, Point p3)
+        {
+            side_r_length = Math.Abs(p3.Y - p2.Y);
+            side_l_length = Math.Abs(p1.Y - p3.Y);
+            bottom_length = Math.Abs(p1.X - p2.X);
+        }
+        public override string ToString() { return $"Perimeter of Triangle is {CalcPerim()} and Area is {CalcArea()}"; }
+
+    }
+    class ShapeAccumulator
+    {
+        List<IShape> shapes = new List<IShape>();
+        IShape MinArea, MaxArea, MinPerim, MaxPerim;
+        double TotalArea, TotalPerim;
+        public ShapeAccumulator()
+        {
+            MinArea = null;
+            MaxArea = null;
+            MinPerim = null;
+            MaxPerim = null;
+            TotalArea = 0;
+            TotalPerim = 0;
+        }
+        public void Add(IShape shape)
+        {
+            shapes.Add(shape);
+            TotalArea += shape.CalcArea();
+            TotalPerim += shape.CalcPerim();
+            if (shapes.Count == 1)
+            {
+                MinArea = shape;
+                MaxArea = shape;
+                MinPerim = shape;
+                MaxPerim = shape;
+                return;
+            }
+            if (MinArea.CalcArea() > shape.CalcArea())
+                MinArea = shape;
+            else if (MaxArea.CalcArea() < shape.CalcArea())
+                MaxArea = shape;
+
+            if (MinPerim.CalcPerim() > shape.CalcPerim())
+                MinPerim = shape;
+            else if (MaxPerim.CalcPerim() < shape.CalcPerim())
+                MaxPerim = shape;
+        }
+        public void AddAll(List<IShape> add)
+        {
+            for (int i = 0; i < add.Count; i++)
+                Add(add[i]);
+        }
+        public IShape GetMaxAreaShape()
+        {
+            return MaxArea;
+        }
+        public IShape GetMinAreaShape()
+        {
+            return MinArea;
+        }
+        public IShape GetMaxPerimShape()
+        {
+            return MaxPerim;
+        }
+        public IShape GetMinPerimShape()
+        {
+            return MinPerim;
+        }
+        public double GetTotalArea()
+        {
+            return TotalArea;
+        }
+        public double GetTotalPerimeter()
+        {
+            return TotalPerim;
+        }
+        public int Count()
+        {
+            return shapes.Count;
+        }
+        public IShape this[int i]
+        {
+            get { return shapes[i]; }
+        }
+    }
+}
---
+++ OOP4_Lab/OOP4_Lab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System;
+
+namespace OOP4_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figures figures = new Figures();
+
+            figures.AddShape(new Square(new Point(0, 0), new Point(0, 10)));
+            figures.AddShape(new Rectangle(new Point(0, 0), new Point(5, 10)));
+            figures.AddShape(new Circle(new Point(0, 0), new Point(6, 0)));
+            figures.AddShape(new Triangle(new Point(0, 0), new Point(6, 0), new Point(3, 5)));
+
+
+            ShapeAccumulator acc = new ShapeAccumulator();
+            acc.Add(figures[0]);
+            acc.AddAll(figures.RetShape());
+            Console.WriteLine(acc.GetMaxAreaShape());
+            Console.WriteLine(acc.GetMaxPerimShape());
+            Console.WriteLine(acc.GetMinAreaShape());
+            Console.WriteLine(acc.GetMinPerimShape());
+            Console.WriteLine(acc.GetTotalArea());
+            Console.WriteLine(acc.GetTotalPerimeter());
+        }
+    }
+}
